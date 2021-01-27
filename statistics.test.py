01ff8a0228891919876a75def3bda1aed4ac3f2b
aswinmath94 <49982968+aswinmath94@users.noreply.github.com>
2021-01-27 20:30:29 +0530

    #1 updates for statistics.test.py

--- statistics.test.py
+++ statistics.test.py
@@ -1,20 +1,22 @@
 import unittest
 import statistics
+import math
 
 class StatsTest(unittest.TestCase):
+
   def test_report_min_max_avg(self):
     computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
     epsilon = 0.001
-    self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
+    self.assertAlmostEqual(computedStats["avg"], 4.525,delta=epsilon)
     self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
 
   def test_avg_is_nan_for_empty_input(self):
     computedStats = statistics.calculateStats([])
-    # All fields of computedStats (average, max, min) must be
-    # nan (not-a-number), as defined in the math package
-    # Design the assert here.
-    # Use nan and isnan in https://docs.python.org/3/library/math.html
+    check = math.isnan(float(computedStats["avg"])) and math.isnan(float(computedStats["max"])) and math.isnan(float(computedStats["min"]))
+    self.assertTrue(check)
+
+
 
   def test_raise_alerts_when_max_above_threshold(self):
     emailAlert = EmailAlert()
@@ -25,5 +27,8 @@ class StatsTest(unittest.TestCase):
     self.assertTrue(emailAlert.emailSent)
     self.assertTrue(ledAlert.ledGlows)
 
+
+
 if __name__ == "__main__":
   unittest.main()
+
